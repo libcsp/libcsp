d2c95376ac479cd5de6efe501fb9856b548901e8
Jeppe Ledet-Pedersen <jeppe@gomspace.com>
2010-09-30 13:33:22 +0000

    Cleaned up a couple of macros and protected with do {} while (0)

--- src/crypto/csp_sha1.c
+++ src/crypto/csp_sha1.c
@@ -32,28 +32,25 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #if CSP_ENABLE_SHA1
 
 /* The SHA1 block size and message digest sizes, in bytes */
-#define SHA1_BLOCKSIZE    64
-#define SHA1_DIGESTSIZE   20
+#define SHA1_BLOCKSIZE    	64
+#define SHA1_DIGESTSIZE   	20
 
 /* Rotate left the hard way (platform optimisations could be done) */
-#define ROL(x,y)	(((x) << (y)) | ((x) >> (32-y)))
+#define ROL(x,y)			(((x) << (y)) | ((x) >> (32-y)))
 
 /* Endian Neutral macros that work on all platforms */
-#define STORE32H(x, y)                                                                     \
-     { (y)[0] = (unsigned char)(((x)>>24)&255); (y)[1] = (unsigned char)(((x)>>16)&255);   \
-       (y)[2] = (unsigned char)(((x)>>8)&255); (y)[3] = (unsigned char)((x)&255); }
+#define STORE32H(x, y) do { (y)[0] = (unsigned char)(((x)>>24)&255); (y)[1] = (unsigned char)(((x)>>16)&255); \
+							(y)[2] = (unsigned char)(((x)>>8)&255); (y)[3] = (unsigned char)((x)&255); } while (0)
 
-#define LOAD32H(x, y)                            \
-     { x = ((unsigned long)((y)[0] & 255)<<24) | \
-           ((unsigned long)((y)[1] & 255)<<16) | \
-           ((unsigned long)((y)[2] & 255)<<8)  | \
-           ((unsigned long)((y)[3] & 255)); }
+#define LOAD32H(x, y) do { x = ((unsigned long)((y)[0] & 0xff) << 24) | \
+							   ((unsigned long)((y)[1] & 0xff) << 16) | \
+							   ((unsigned long)((y)[2] & 0xff) <<  8) | \
+							   ((unsigned long)((y)[3] & 0xff) <<  0); } while (0)
 
-#define STORE64H(x, y)                                                                     \
-   { (y)[0] = (unsigned char)(((x)>>56)&255); (y)[1] = (unsigned char)(((x)>>48)&255);     \
-     (y)[2] = (unsigned char)(((x)>>40)&255); (y)[3] = (unsigned char)(((x)>>32)&255);     \
-     (y)[4] = (unsigned char)(((x)>>24)&255); (y)[5] = (unsigned char)(((x)>>16)&255);     \
-     (y)[6] = (unsigned char)(((x)>>8)&255); (y)[7] = (unsigned char)((x)&255); }
+#define STORE64H(x, y) do {	(y)[0] = (unsigned char)(((x) >> 56) & 0xff); (y)[1] = (unsigned char)(((x) >> 48) & 0xff); \
+							(y)[2] = (unsigned char)(((x) >> 40) & 0xff); (y)[3] = (unsigned char)(((x) >> 32) & 0xff); \
+							(y)[4] = (unsigned char)(((x) >> 24) & 0xff); (y)[5] = (unsigned char)(((x) >> 16) & 0xff); \
+							(y)[6] = (unsigned char)(((x) >>  8) & 0xff); (y)[7] = (unsigned char)(((x) >>  0) & 0xff); } while (0)
 
 #define MIN(x, y) (((x)<(y))?(x):(y))
 
@@ -63,10 +60,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #define F2(x,y,z)  ((x & y) | (z & (x | y)))
 #define F3(x,y,z)  (x ^ y ^ z)
 
-#define FF_0(a,b,c,d,e,i) e = (ROL(a, 5) + F0(b,c,d) + e + W[i] + 0x5a827999UL); b = ROL(b, 30);
-#define FF_1(a,b,c,d,e,i) e = (ROL(a, 5) + F1(b,c,d) + e + W[i] + 0x6ed9eba1UL); b = ROL(b, 30);
-#define FF_2(a,b,c,d,e,i) e = (ROL(a, 5) + F2(b,c,d) + e + W[i] + 0x8f1bbcdcUL); b = ROL(b, 30);
-#define FF_3(a,b,c,d,e,i) e = (ROL(a, 5) + F3(b,c,d) + e + W[i] + 0xca62c1d6UL); b = ROL(b, 30);
+#define FF_0(a,b,c,d,e,i) do {e = (ROL(a, 5) + F0(b,c,d) + e + W[i] + 0x5a827999UL); b = ROL(b, 30);} while (0)
+#define FF_1(a,b,c,d,e,i) do {e = (ROL(a, 5) + F1(b,c,d) + e + W[i] + 0x6ed9eba1UL); b = ROL(b, 30);} while (0)
+#define FF_2(a,b,c,d,e,i) do {e = (ROL(a, 5) + F2(b,c,d) + e + W[i] + 0x8f1bbcdcUL); b = ROL(b, 30);} while (0)
+#define FF_3(a,b,c,d,e,i) do {e = (ROL(a, 5) + F3(b,c,d) + e + W[i] + 0xca62c1d6UL); b = ROL(b, 30);} while (0)
 
 /* The structure for storing SHA1 info */
 struct sha1_state {
