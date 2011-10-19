d211e7c04005aa65c2e68fa738f68d9d3b267997
Johan De Claville Christiansen <johan@gomspace.com>
2011-10-19 10:51:26 +0200

    Added a bit of doxygen to csp.h

--- include/csp/csp.h
+++ include/csp/csp.h
@@ -180,13 +180,13 @@ typedef union {
  * have buffer reuse
  */
 typedef struct __attribute__((__packed__)) {
-	uint8_t padding[CSP_PADDING_BYTES];   	// Interface dependent padding
-	uint16_t length;			// Length field must be just before CSP ID
-	csp_id_t id;				// CSP id must be just before data
+	uint8_t padding[CSP_PADDING_BYTES];   	/**< Interface dependent padding */
+	uint16_t length;			/**< Length field must be just before CSP ID */
+	csp_id_t id;				/**< CSP id must be just before data */
 	union {
-		uint8_t data[0];		// This just points to the rest of the buffer, without a size indication.
-		uint16_t data16[0];		// The data 16 and 32 types makes it easy to reference an integer (properly aligned)
-		uint32_t data32[0];		// - without the compiler warning about strict aliasing rules.
+		uint8_t data[0];		/**< This just points to the rest of the buffer, without a size indication. */
+		uint16_t data16[0];		/**< The data 16 and 32 types makes it easy to reference an integer (properly aligned) */
+		uint32_t data32[0];		/**< without the compiler warning about strict aliasing rules. */
 	};
 } csp_packet_t;
 
