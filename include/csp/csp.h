f9d5b3070404ac6ab573e89325676c69b3691101
Jeppe Ledet-Pedersen <jeppe@gomspace.com>
2011-10-24 16:30:21 +0200

    Remove unused CSP_STATS port

--- include/csp/csp.h
+++ include/csp/csp.h
@@ -59,7 +59,6 @@ enum csp_reserved_ports_e {
 	CSP_REBOOT		  	= 4,
 	CSP_BUF_FREE		= 5,
 	CSP_UPTIME			= 6,
-	CSP_STATS			= 7,
 	CSP_ANY			 	= (CSP_MAX_BIND_PORT + 1),
 	CSP_PROMISC		 	= (CSP_MAX_BIND_PORT + 2)
 };
