4103827f9662155ef2ef0a00eef14a78a439b1d9
Johan De Claville Christiansen <johan@gomspace.com>
2012-09-27 10:30:56 +0200

    Fix include files

--- src/transport/csp_udp.c
+++ src/transport/csp_udp.c
@@ -18,8 +18,6 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include <stdio.h>
-
 #include <csp/csp.h>
 #include "../arch/csp_queue.h"
 #include "../csp_port.h"
