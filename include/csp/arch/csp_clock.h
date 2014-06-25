a7e32273531865c8bf96c4d59d9d4a65df0a8ff6
Johan De Claville Christiansen <johan@gomspace.com>
2014-06-25 16:41:59 +0200

    Forgot to add csp_clock.h

---
+++ include/csp/arch/csp_clock.h
@@ -0,0 +1,43 @@
+/*
+Cubesat Space Protocol - A small network-layer protocol designed for Cubesats
+Copyright (C) 2012 Gomspace ApS (http://www.gomspace.com)
+Copyright (C) 2012 AAUSAT3 Project (http://aausat3.space.aau.dk) 
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _CSP_CLOCK_H_
+#define _CSP_CLOCK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+typedef struct {
+	uint32_t tv_sec;
+	uint32_t tv_nsec;
+} timestamp_t;
+
+/* User functions required */
+extern void clock_get_time(timestamp_t * time);
+extern void clock_set_time(timestamp_t * time);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif // _CSP_CLOCK_H_
