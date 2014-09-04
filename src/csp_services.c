43c1578c40f91bc5858ed8cc065c3d6e5aabb0c4
Gomspace <cspweb.gomspace@gmail.com>
2014-09-04 12:57:32 +0200

    Fix NULL char at end of line for new RPS function

--- src/csp_services.c
+++ src/csp_services.c
@@ -140,7 +140,7 @@ void csp_ps(uint8_t node, uint32_t timeout) {
 	packet->data[0] = 0x55;
 	packet->length = 1;
 
-	printf("PS node %"PRIu8": ", node);
+	printf("PS node %"PRIu8": \r\n", node);
 
 	/* Try to send frame */
 	if (!csp_send(conn, packet, 0))
@@ -153,7 +153,8 @@ void csp_ps(uint8_t node, uint32_t timeout) {
 		if (packet == NULL)
 			break;
 
-		/* We have a reply */
+		/* We have a reply, add our own NULL char */
+		packet->data[packet->length] = 0;
 		printf("%s", packet->data);
 
 	}
