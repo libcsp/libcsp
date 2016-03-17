730b42692e144fc9de2254e7f7d585e972cd231b
Magne Normann <magne.normann@atmel.com>
2016-03-17 23:14:20 +0100

    Added csp_nuts.h with socket information

---
+++ include/csp/csp_nuts.h
@@ -0,0 +1,9 @@
+/* This header file contains all csp information specific for NUTS */
+
+#define OBC_ADDR			0x1
+
+#define HOUSEKEEPER_RX_PORT	0x10
+#define TIMEKEEPER_RX_PORT	0x11
+#define TIMEKEEPER_RX_PORT_RDP 0x14
+#define EVENT_MAN_RX_PORT	0x12
+#define LOGGER_RX_PORT		0x13
