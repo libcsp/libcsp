5979b47ff30347e7450b4d97d6bb8ad464b4d9b5
Jeppe Ledet-Pedersen <jlp@gomspace.com>
2016-06-15 15:18:18 +0200

    WIP: more csperf

--- utils/csperf.c
+++ utils/csperf.c
@@ -35,6 +35,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <csp/csp.h>
 #include <csp/csp_endian.h>
 #include <csp/interfaces/csp_if_can.h>
+#include <csp/arch/csp_thread.h>
+#include <csp/arch/csp_time.h>
 
 unsigned int timeout_ms = 1000;
 unsigned int data_size = 100;
@@ -42,6 +44,7 @@ bool should_stop = false;
 unsigned int runtime = 10;
 unsigned int max_frames = 0;
 unsigned int bandwidth = 1000000; /* bits/s */
+unsigned int update_interval = 1;
 
 #define CSPERF_TYPE_DATA 1
 #define CSPERF_TYPE_DONE 2
@@ -52,16 +55,16 @@ struct csperf_packet {
 	uint8_t data[0];
 } __attribute__ ((packed));
 
-uint64_t get_nanosecs(void)
+static uint64_t get_us(void)
 {
-	struct timespec now;
+       struct timespec now;
 
-	clock_gettime(CLOCK_MONOTONIC, &now);
+       clock_gettime(CLOCK_MONOTONIC, &now);
 
-	return now.tv_sec * 1000000000UL + now.tv_nsec;
+       return now.tv_sec * 1000000UL + now.tv_nsec / 1000;
 }
 
-void fill_pattern(uint8_t *dst, size_t len)
+static void fill_pattern(uint8_t *dst, size_t len)
 {
 	int i;
 	uint8_t b = 0x01;
@@ -70,7 +73,7 @@ void fill_pattern(uint8_t *dst, size_t len)
 		*dst++ = b++;
 }
 
-bool check_pattern(uint8_t *dst, size_t len)
+static bool check_pattern(uint8_t *dst, size_t len)
 {
 	int i;
 	uint8_t b = 0x01;
@@ -83,56 +86,117 @@ bool check_pattern(uint8_t *dst, size_t len)
 	return true;
 }
 
-void handle_connection(csp_conn_t *conn)
+static void pretty_bytes(uint32_t bytes, char *out)
+{
+	float bbytes;
+	char *unit = "";
+
+	if (bytes > 1000000000) {
+		bbytes = bytes / 1000000000.0;
+		unit = "G";
+	} else if (bytes > 1000000) {
+		bbytes = bytes / 1000000.0;
+		unit = "M";
+	} else if (bytes > 1000) {
+		bbytes = bytes / 1000.0;
+		unit = "K";
+	} else {
+		bbytes = bytes;
+	}
+
+	sprintf(out, "%5.1f%s", bbytes, unit);
+}
+
+struct csperf_state {
+	uint32_t time;
+
+	uint32_t bytes;
+	uint32_t received;
+	uint32_t lost;
+	uint32_t corrupt;
+};
+
+static void pretty_progress(uint32_t start, uint32_t last, uint32_t now, struct csperf_state state)
+{
+	char bytestr[10];
+	char bitstr[10];
+	float diff_start = (last - start) / 1000.0;
+	float diff_last = (now - start) / 1000.0;
+
+	pretty_bytes(state.bytes, bytestr);
+	pretty_bytes(state.bytes * 8 / (diff_last - diff_start), bitstr);
+	printf("[ ] %.1f - %.1f  %s  %sbit/s %u/%u/%u\n",
+	       diff_start, diff_last, bytestr, bitstr, state.received, state.lost, state.corrupt);
+}
+
+static void handle_connection(csp_conn_t *conn)
 {
 	csp_packet_t *packet;
 	struct csperf_packet *pp;
 
+	struct csperf_state total = {0}, last = {0};
+
 	uint32_t seq;
 	uint32_t last_seq = 0;
-	bool valid;
 
-	uint32_t received = 0;
-	uint32_t lost = 0;
-	uint32_t corrupt = 0;
+	uint32_t now = 0;
+	uint32_t last_start_time = 0;
+	uint32_t start_time;
 
-	uint32_t last_update_seq = 0;
-	uint64_t last_update_time = 0;
-
-	printf("Connection from %hhu:%hhu\n",
+	printf("[ ] connected with %hhu port %hhu\n",
 	       csp_conn_src(conn), csp_conn_sport(conn));
 
+	start_time = csp_get_ms();
+	last_start_time = start_time;
 	while (1) {
 		packet = csp_read(conn, timeout_ms);
-		if (!packet) {
-			printf("Timeout\n");
+		if (!packet)
 			break;
-		}
 
 		if (packet->length != sizeof(*pp) + data_size)
 			break;
 
 		pp = (struct csperf_packet *) packet->data;
 		seq = csp_ntoh32(pp->seq);
-		valid = check_pattern(pp->data, data_size);
 
-		received++;
+		/* Update counters */
+		last.received += 1;
+		last.bytes += packet->length;
 
 		if (last_seq > 0 && seq != last_seq + 1)
-			lost += seq - last_seq - 1;
+			last.lost += seq - last_seq - 1;
 
-		if (!valid) {
-			printf("corrupted packet received!\n");
-			corrupt++;
-		}
+		if (!check_pattern(pp->data, data_size))
+			last.corrupt++;
+
+		last_seq = seq;
 
 		csp_buffer_free(packet);
+
+		/* Update progress */
+		now = csp_get_ms();
+		if (now >= last_start_time + 1000 * update_interval) {
+			total.bytes += last.bytes;
+			total.received += last.received;
+			total.lost += last.lost;
+			total.corrupt += last.corrupt;
+
+			pretty_progress(start_time, last_start_time, now, last);
+			last_start_time = now;
+
+			memset(&last, 0, sizeof(last));
+		}
 	}
 
-	printf("Received %u packets\n", received);
+	total.bytes += last.bytes;
+	total.received += last.received;
+	total.lost += last.lost;
+	total.corrupt += last.corrupt;
+
+	pretty_progress(start_time, start_time, now, total);
 }
 
-int run_server(uint8_t port)
+static int run_server(uint8_t port)
 {
 	csp_socket_t *socket;
 	csp_conn_t *conn;
@@ -142,7 +206,9 @@ int run_server(uint8_t port)
 	csp_bind(socket, port);
 	csp_listen(socket, 10);
 
+	printf("------------------------------------------------------------\n");
 	printf("Server listening on port %hhu\n", port);
+	printf("------------------------------------------------------------\n");
 
 	while (1) {
 		if ((conn = csp_accept(socket, 250)) == NULL) {
@@ -159,17 +225,21 @@ int run_server(uint8_t port)
 	return 0;
 }
 
-int run_client(uint8_t server, uint8_t port)
+static int run_client(uint8_t server, uint8_t port)
 {
 	csp_conn_t *conn;
 	csp_packet_t *packet;
 	struct csperf_packet *pp;
 
 	uint32_t seq = 0;
-	uint64_t start_time, now;
-	unsigned int sleep_time;
+	uint64_t start_time, transmit_time;
+	int64_t interval_target;
 
-	printf("Client connecting to %hhu port %hhu\n", server, port);
+	int64_t interval = 0, adjust = 0;
+
+	printf("------------------------------------------------------------\n");
+	printf("Client connecting to %hhu, port %hhu\n", server, port);
+	printf("------------------------------------------------------------\n");
 
 	conn = csp_connect(CSP_PRIO_NORM, server, port, timeout_ms, CSP_O_NONE);
 	if (!conn) {
@@ -177,21 +247,25 @@ int run_client(uint8_t server, uint8_t port)
 		return -ENOTCONN;
 	}
 
-	/* Calculate time between packets in us for target speed */
-	sleep_time = (sizeof(*pp) + data_size) * 8 * 1000000 / bandwidth;
-	printf("Sleeping %u us between packets\n", sleep_time);
+	/* Calculate target time between packets in us for target speed */
+	interval_target = (sizeof(*pp) + data_size) * 8 * 1000000 / bandwidth;
+	printf("%lu us between packets\n", interval_target);
 
-	start_time = get_nanosecs();
+	start_time = get_us();
 
 	while (1) {
+		/* Log start of transmission cycle */
+		transmit_time = get_us();
+
+		int64_t sleep_time = interval_target - adjust;
+		if (sleep_time < 0)
+			sleep_time = 0;
+
 		if (should_stop)
 			break;
 
-		now = get_nanosecs();
-		if (now >= start_time + runtime * 1000000000UL) {
-			printf("done\n");
+		if (get_us() > start_time + runtime * 1000000U)
 			break;
-		}
 
 		packet = csp_buffer_get(sizeof(*pp) + data_size);
 		if (!packet) {
@@ -210,7 +284,16 @@ int run_client(uint8_t server, uint8_t port)
 			break;
 		}
 
+		/* Sleep until next transmission */
 		usleep(sleep_time);
+
+		interval = (int64_t)get_us() - (int64_t)transmit_time;
+
+		/* Adjust next sleep with the time we actually slept last
+		 * cycle. Correct with the adjustment value from last cycle to
+		 * avoid double compensation */
+		adjust = (interval - interval_target) + adjust;
+
 	}
 
 	printf("Sent %u packets\n", seq);
@@ -218,18 +301,18 @@ int run_client(uint8_t server, uint8_t port)
 	return 0;
 }
 
-void sighandler(int signo)
+static void sighandler(int signo)
 {
 	if (signo == SIGINT)
 		should_stop = true;
 }
 
-void usage(const char *argv0)
+static void usage(const char *argv0)
 {
 	printf("usage: %s [-s|-c host]\n", argv0);
 }
 
-void help(const char *argv0)
+static void help(const char *argv0)
 {
 	usage(argv0);
 	printf("  -a, --address ADDRESS    Set address\r\n");
