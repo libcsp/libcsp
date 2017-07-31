4cd8a300bde86888a26da423cc3608c34348777b
Johan de Claville Christiansen <johan@gomspace.com>
2017-07-31 15:07:14 +0200

    Got rid of CSP_CONN_QUEUE_LENGTH

--- src/csp_io.c
+++ src/csp_io.c
@@ -99,7 +99,7 @@ csp_socket_t * csp_socket(uint32_t opts) {
 	/* If connectionless, init the queue to a pre-defined size
 	 * if not, the user must init the queue using csp_listen */
 	if (opts & CSP_SO_CONN_LESS) {
-		sock->socket = csp_queue_create(CSP_CONN_QUEUE_LENGTH, sizeof(csp_packet_t *));
+		sock->socket = csp_queue_create(csp_conf.conn_queue_length, sizeof(csp_packet_t *));
 		if (sock->socket == NULL)
 			return NULL;
 	} else {
