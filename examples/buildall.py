e5705c3cbd7e61fb64c6db2850b2c4eda451d70c
perwulff <pwu@gomspace.com>
2020-01-13 15:13:03 +0100

    Updadted example code and documentation.

--- examples/buildall.py
+++ examples/buildall.py
@@ -2,32 +2,53 @@
 # encoding: utf-8
 
 import subprocess
+import sys
 
 
-options = [
+os = 'posix'  ## default OS
+options = sys.argv[1:]
+if (len(options) > 0) and not options[0].startswith('--'):
+    os = options[0]
+    options = options[1:]
+
+options += [
+    '--with-os=' + os,
     '--enable-rdp',
-    '--enable-qos',
     '--enable-promisc',
     '--enable-crc32',
     '--enable-hmac',
     '--enable-xtea',
     '--enable-dedup',
-    '--enable-if-i2c',
-    '--enable-if-kiss',
-    '--enable-if-can',
+    '--with-loglevel=debug',
 ]
 
-linux_options = [
+waf = ['./waf']
+if os in ['posix']:
+    options += [
     '--enable-bindings',
     '--enable-python3-bindings',
     '--enable-can-socketcan',
     '--with-driver-usart=linux',
-    '--with-os=posix',
     '--enable-if-zmqhub'
 ]
 
-# Build on linux
-subprocess.check_call(['./waf', 'distclean', 'configure', 'build'] + options + linux_options +
-                      ['--enable-init-shutdown', '--with-rtable=cidr', '--disable-stlib', '--disable-output'])
-subprocess.check_call(['./waf', 'distclean', 'configure', 'build'] + options + linux_options +
+if os in ['macosx']:
+    options += [
+        '--with-driver-usart=linux',
+    ]
+
+if os in ['windows']:
+    options += [
+        # '--with-driver-usart=windows',
+    ]
+    waf = ['python', '-x', 'waf']
+
+# Build
+waf += ['distclean', 'configure', 'build']
+print("Waf build command:", waf)
+subprocess.check_call(waf + options +
+                      ['--enable-qos', '--enable-init-shutdown', '--with-rtable=cidr', '--disable-stlib',
+                       '--disable-output'])
+if os not in ['windows']:  # TODO fix examples for WIndows
+    subprocess.check_call(waf + options +
                       ['--enable-examples'])
