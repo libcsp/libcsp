966e70c80b4564bff1e04269bcb090c5480be8b1
Marcos <maekos@gmail.com>
2020-02-18 17:49:31 -0300

    feat: create csp_autoconfig.h

---
+++ include/csp/csp_autoconfig.h
@@ -0,0 +1,34 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
+#define _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
+
+#define GIT_REV "v1.2-3-g44bcdf7"
+/* #define CSP_FREERTOS 1 */
+#define CSP_POSIX 1
+/* #undef CSP_WINDOWS */
+/* #undef CSP_MACOSX */
+#define CSP_DEBUG 1
+#define CSP_VERBOSE 1
+/* #undef CSP_USE_RDP */
+#define CSP_USE_CRC32 1
+/* #undef CSP_USE_HMAC */
+/* #undef CSP_USE_XTEA */
+/* #undef CSP_USE_PROMISC */
+/* #undef CSP_USE_QOS */
+#define CSP_CONN_MAX 5
+#define CSP_CONN_QUEUE_LENGTH 10
+#define CSP_FIFO_INPUT 10
+#define CSP_MAX_BIND_PORT 12
+#define CSP_RDP_MAX_WINDOW 20
+#define CSP_PADDING_BYTES 8
+#define CSP_LOG_LEVEL_DEBUG 1
+#define CSP_LOG_LEVEL_INFO 1
+#define CSP_LOG_LEVEL_WARN 1
+#define CSP_LOG_LEVEL_ERROR 1
+#define CSP_LITTLE_ENDIAN 1
+/* #undef CSP_BIG_ENDIAN */
+#define CSP_HAVE_STDBOOL_H 1
+#define CSP_USE_DEBUG_COLOR 0
+
+#endif /* _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF */
