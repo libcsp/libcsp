10775ae87de2335cd8e2eac9fc110ddf55e70ce9
Yasushi SHOJI <yashi@spacecubics.com>
2021-07-28 18:33:38 +0900

    examples/buildall.py: Put waf specific code in a function
    This commit moves all code into a function `build_with_waf()`.  This
    is a preparation of having yet another build system, meson, in this
    script.
    
    The diff is huge but if you set "ignore all space" you see what it
    really does.
    
    No functionality has been changed.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- examples/buildall.py
+++ examples/buildall.py
@@ -5,48 +5,57 @@ import subprocess
 import sys
 
 
-target_os = 'posix'  # default OS
-options = sys.argv[1:]
-if (len(options) > 0) and not options[0].startswith('--'):
-    target_os = options[0]
-    options = options[1:]
+def build_with_waf():
+    target_os = 'posix'  # default OS
+    options = sys.argv[1:]
+    if (len(options) > 0) and not options[0].startswith('--'):
+        target_os = options[0]
+        options = options[1:]
 
-options += [
-    '--with-os=' + target_os,
-    '--enable-rdp',
-    '--enable-promisc',
-    '--enable-crc32',
-    '--enable-hmac',
-    '--enable-xtea',
-    '--enable-dedup',
-    '--with-loglevel=debug',
-    '--enable-debug-timestamp'
-]
-
-waf = ['./waf']
-if target_os in ['posix']:
     options += [
-        '--enable-python3-bindings',
-        '--enable-can-socketcan',
-        '--with-driver-usart=linux',
-        '--enable-if-zmqhub',
-        '--enable-shlib'
+        '--with-os=' + target_os,
+        '--enable-rdp',
+        '--enable-promisc',
+        '--enable-crc32',
+        '--enable-hmac',
+        '--enable-xtea',
+        '--enable-dedup',
+        '--with-loglevel=debug',
+        '--enable-debug-timestamp'
     ]
 
-if target_os in ['macosx']:
-    options += [
-        '--with-driver-usart=linux',
-    ]
+    waf = ['./waf']
+    if target_os in ['posix']:
+        options += [
+            '--enable-python3-bindings',
+            '--enable-can-socketcan',
+            '--with-driver-usart=linux',
+            '--enable-if-zmqhub',
+            '--enable-shlib'
+        ]
 
-if target_os in ['windows']:
-    options += [
-        '--with-driver-usart=windows',
-    ]
-    waf = ['python', '-x', 'waf']
+    if target_os in ['macosx']:
+        options += [
+            '--with-driver-usart=linux',
+        ]
 
-# Build
-waf += ['distclean', 'configure', 'build']
-print("Waf build command:", waf)
-subprocess.check_call(waf + options +
-                      ['--with-rtable=cidr', '--disable-stlib', '--disable-output'])
-subprocess.check_call(waf + options + ['--enable-examples'])
+    if target_os in ['windows']:
+        options += [
+            '--with-driver-usart=windows',
+        ]
+        waf = ['python', '-x', 'waf']
+
+    # Build
+    waf += ['distclean', 'configure', 'build']
+    print("Waf build command:", waf)
+    subprocess.check_call(waf + options +
+                          ['--with-rtable=cidr', '--disable-stlib', '--disable-output'])
+    subprocess.check_call(waf + options + ['--enable-examples'])
+
+
+def main():
+    build_with_waf()
+
+
+if __name__ == '__main__':
+    main()
