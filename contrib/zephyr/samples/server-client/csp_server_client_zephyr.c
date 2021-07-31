88d561c38c4929afd7fd32518f4e7cda36a802d4
Yasushi SHOJI <yashi@spacecubics.com>
2021-07-31 20:59:03 +0900

    zephyr: samples: Add server client sample
    Add a custom Zephyr version of csp_server_client.  This version is a
    copy of the original examples/csp_server_client.c with a few fix for
    Zephyr.  Basically the fixes are removal of unsupported features.  You
    can see the changes by diffing examples/csp_server_client.c against
    main.c without white space changes.
    
     - Zephyr doesn't have getopt().
     - Zephyr currently doesn't support CAN nor ZMQ.
     - Zephyr's main() is `void main(void)` instead of `int main(int, char **)`.
     - #if CSP_HAVE_STDIO is needed.  It's handled by the header file.
     - This version enforces the `test_mode`.
     - arch specific functions doesn't return anything.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ contrib/zephyr/samples/server-client/csp_server_client_zephyr.c
@@ -0,0 +1,66 @@
+#include <zephyr.h>
+#include <csp/csp.h>
+
+void server(void);
+void client(void);
+
+#define ROUTER_STACK_SIZE 200
+#define SERVER_STACK_SIZE 400
+#define CLIENT_STACK_SIZE 400
+#define ROUTER_PRIO 0
+#define SERVER_PRIO 0
+#define CLIENT_PRIO 0
+
+static void * router_task(void * param) {
+
+	/* Here there be routing */
+	while (1) {
+		csp_route_work();
+	}
+
+	return NULL;
+}
+
+static void * server_task(void * p1, void * p2, void * p3) {
+
+	ARG_UNUSED(p1);
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	server();
+
+	return NULL;
+}
+
+static void * client_task(void * p1, void * p2, void * p3) {
+
+	ARG_UNUSED(p1);
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	client();
+
+	return NULL;
+}
+
+K_THREAD_DEFINE(router_id, ROUTER_STACK_SIZE,
+				router_task, NULL, NULL, NULL,
+				ROUTER_PRIO, 0, K_TICKS_FOREVER);
+K_THREAD_DEFINE(server_id, SERVER_STACK_SIZE,
+				server_task, NULL, NULL, NULL,
+				SERVER_PRIO, 0, K_TICKS_FOREVER);
+K_THREAD_DEFINE(client_id, CLIENT_STACK_SIZE,
+				client_task, NULL, NULL, NULL,
+				CLIENT_PRIO, 0, K_TICKS_FOREVER);
+
+void router_start(void) {
+	k_thread_start(router_id);
+}
+
+void server_start(void) {
+	k_thread_start(server_id);
+}
+
+void client_start(void) {
+	k_thread_start(client_id);
+}
