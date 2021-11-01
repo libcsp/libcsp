14e4dfc8ac1e4fbaf2685a442aca2b69a897dd7c
Johan de Claville Christiansen <johan@space-inventor.com>
2021-11-01 14:16:00 +0100

    Updated zmqproxy to latest version

--- examples/zmqproxy.c
+++ examples/zmqproxy.c
@@ -5,21 +5,44 @@
 #include <pthread.h>
 
 #include <csp/csp.h>
+#include <csp/arch/csp_system.h>
+
+int csp_id_strip(csp_packet_t * packet);
+int csp_id_setup_rx(csp_packet_t * packet);
+extern csp_conf_t csp_conf;
+
+int debug = 0;
+const char * sub_str = "tcp://0.0.0.0:6000";
+const char * pub_str = "tcp://0.0.0.0:7000";
+char * logfile_name = NULL;
+FILE * logfile;
 
 static void * task_capture(void * ctx) {
 
-	int ret;
+    int ret;
+
+	csp_sys_set_color(COLOR_BLUE);
+	printf("Capture/logging task listening on %s\n", sub_str);
+	csp_sys_set_color(COLOR_RESET);
 
 	/* Subscriber (RX) */
 	void * subscriber = zmq_socket(ctx, ZMQ_SUB);
-	ret = zmq_connect(subscriber, "tcp://localhost:7000");
-	assert(ret == 0);
-	zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
-	assert(ret == 0);
+	ret = zmq_connect(subscriber, pub_str);
+    assert(ret == 0);
+	ret = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
+    assert(ret == 0);
 
 	/* Allocated 'raw' CSP packet */
 	csp_packet_t * packet = malloc(1024);
 	assert(packet != NULL);
+
+	if (logfile_name) {
+		logfile = fopen(logfile_name, "a+");
+		if (logfile == NULL) {
+			printf("Unable to open logfile %s\n", logfile_name);
+			exit(-1);
+		}
+	}
 
 	while (1) {
 		zmq_msg_t msg;
@@ -40,44 +63,66 @@ static void * task_capture(void * ctx) {
 			continue;
 		}
 
-		/* Copy the data from zmq to csp */
-		char * satidptr = ((char *)&packet->id) - 1;
-		memcpy(satidptr, zmq_msg_data(&msg), datalen);
-		packet->length = datalen - sizeof(packet->id) - 1;
+		/* Copy to packet */
+		csp_id_setup_rx(packet);
+		memcpy(packet->frame_begin, zmq_msg_data(&msg), datalen);
+		packet->frame_length = datalen;
 
-		csp_log_packet("Input: Src %u, Dst %u, Dport %u, Sport %u, Pri %u, Flags 0x%02X, Size %" PRIu16,
-					   packet->id.src, packet->id.dst, packet->id.dport,
-					   packet->id.sport, packet->id.pri, packet->id.flags, packet->length);
+		/* Parse header */
+		csp_id_strip(packet);
+
+		/* Print header data */
+		csp_sys_set_color(COLOR_GREEN);
+		printf("Packet: Src %u, Dst %u, Dport %u, Sport %u, Pri %u, Flags 0x%02X, Size %" PRIu16 "\n",
+			   packet->id.src, packet->id.dst, packet->id.dport,
+			   packet->id.sport, packet->id.pri, packet->id.flags, packet->length);
+		csp_sys_set_color(COLOR_RESET);
+
+		if (logfile) {
+			const char * delimiter = "--------\n";
+			fwrite(delimiter, sizeof(delimiter), 1, logfile);
+			fwrite(packet->frame_begin, packet->frame_length, 1, logfile);
+			fflush(logfile);
+		}
 
 		zmq_msg_close(&msg);
 	}
-
-	return NULL;
 }
 
 int main(int argc, char ** argv) {
 
-	int ret;
+    int ret;
+	csp_conf.version = 2;
 
-	csp_debug_level_t debug_level = CSP_PACKET;
 	int opt;
-	while ((opt = getopt(argc, argv, "d:h")) != -1) {
+	while ((opt = getopt(argc, argv, "dhv:s:p:f:")) != -1) {
 		switch (opt) {
 			case 'd':
-				debug_level = atoi(optarg);
+				debug = 1;
+				break;
+			case 'v':
+				csp_conf.version = atoi(optarg);
+				break;
+			case 's':
+				sub_str = optarg;
+				break;
+			case 'p':
+				pub_str = optarg;
+				break;
+			case 'f':
+				logfile_name = optarg;
 				break;
 			default:
 				printf(
 					"Usage:\n"
-					" -d <debug-level> debug level, 0 - 6\n");
+					" -d \t\tEnable debug\n"
+					" -v VERSION\tcsp version\n"
+					" -s SUB_STR\tsubscriber port: tcp://localhost:7000\n"
+					" -p PUB_STR\tpublisher  port: tcp://localhost:6000\n"
+					" -f LOGFILE\tLog to this file\n");
 				exit(1);
 				break;
 		}
-	}
-
-	/* enable/disable debug levels */
-	for (csp_debug_level_t i = 0; i <= CSP_LOCK; ++i) {
-		csp_debug_set_level(i, (i <= debug_level) ? true : false);
 	}
 
 	void * ctx = zmq_ctx_new();
@@ -85,21 +130,25 @@ int main(int argc, char ** argv) {
 
 	void * frontend = zmq_socket(ctx, ZMQ_XSUB);
 	assert(frontend);
-	ret = zmq_bind(frontend, "tcp://*:6000");
+    ret = zmq_bind(frontend, sub_str);
 	assert(ret == 0);
+	csp_sys_set_color(COLOR_BLUE);
+	printf("Subscriber task listening on %s\n", sub_str);
 
 	void * backend = zmq_socket(ctx, ZMQ_XPUB);
 	assert(backend);
-	ret = zmq_bind(backend, "tcp://*:7000");
+    ret = zmq_bind(backend, pub_str);
 	assert(ret == 0);
+	printf("Publisher task listening on %s\n", pub_str);
 
 	pthread_t capworker;
 	pthread_create(&capworker, NULL, task_capture, ctx);
 
-	csp_log_info("Starting ZMQproxy");
+	csp_sys_set_color(COLOR_RESET);
+
 	zmq_proxy(frontend, backend, NULL);
 
-	csp_log_info("Closing ZMQproxy");
+	printf("Closing ZMQproxy");
 	zmq_ctx_destroy(ctx);
 
 	return 0;
