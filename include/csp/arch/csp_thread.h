49245a69a9ba0147258cb6566eb51916c9c4bc72
Yasushi SHOJI <yashi@spacecubics.com>
2021-11-04 20:33:18 +0900

    csp_thread: posix: Remove thread_create
    Now all csp_posix_thread_create users are gone.  Remove it.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- include/csp/arch/csp_thread.h
+++ include/csp/arch/csp_thread.h
@@ -10,19 +10,6 @@
 
 #if (CSP_POSIX || __DOXYGEN__)
 #include <pthread.h>
-int csp_posix_thread_create(void *(* func)(void *), const char * const name, unsigned int stack_size, void * parameter, unsigned int priority, pthread_t * handle);
-/**
-   Create thread (task).
-
-   @param[in] func thread function
-   @param[in] name name of thread, supported on: FreeRTOS.
-   @param[in] stack_size stack size, supported on: posix (bytes), FreeRTOS (words, word = 4 bytes).
-   @param[in] parameter parameter for thread function.
-   @param[in] priority thread priority, supported on: FreeRTOS.
-   @param[out] handle reference to created thread.
-   @return #CSP_ERR_NONE on success, otherwise an error code.
-*/
-
 #elif (CSP_MACOSX)
 #include <pthread.h>
 int csp_macosx_thread_create(void *(*func)(void *), const char * const name, unsigned int stack_size, void * parameter, unsigned int priority, pthread_t * handle);
--- src/arch/posix/csp_thread.c
+++ src/arch/posix/csp_thread.c
@@ -6,36 +6,6 @@
 #include <time.h>
 #include <errno.h>
 
-int csp_posix_thread_create(void * (*routine)(void *), const char * const thread_name, unsigned int stack_size, void * parameters, unsigned int priority, pthread_t * return_handle) {
-
-	pthread_attr_t attributes;
-	if (pthread_attr_init(&attributes) != 0) {
-		return CSP_ERR_NOMEM;
-	}
-	// if stack size is 0, use default stack size
-	if (stack_size) {
-		unsigned int min_stack_size = PTHREAD_STACK_MIN;  // use at least one memory
-		while (min_stack_size < stack_size) {             // must reach at least the provided size
-			min_stack_size += PTHREAD_STACK_MIN;          // keep memory page boundary (some systems may fail otherwise))
-		}
-		pthread_attr_setstacksize(&attributes, min_stack_size);
-	}
-	pthread_attr_setdetachstate(&attributes, PTHREAD_CREATE_DETACHED);  // no need to join with thread to free its resources
-
-	pthread_t handle;
-	int return_code = pthread_create(&handle, &attributes, routine, parameters);
-	pthread_attr_destroy(&attributes);
-
-	if (return_code != 0) {
-		return CSP_ERR_NOMEM;
-	}
-	if (return_handle) {
-		*return_handle = handle;
-	}
-
-	return CSP_ERR_NONE;
-}
-
 void csp_sleep_ms(unsigned int time_ms) {
 
 	struct timespec req, rem;
