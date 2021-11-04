c455c3c7a4ac298036610d44aa27b0271344b796
Yasushi SHOJI <yashi@spacecubics.com>
2021-11-04 19:46:08 +0900

    csp_thread: zephyr: Remove thread_create_static
    Now there is no user for csp_zephyr_thread_create_static.  Remove it.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- include/csp/arch/csp_thread.h
+++ include/csp/arch/csp_thread.h
@@ -40,10 +40,6 @@ int csp_windows_thread_create(unsigned int (*func)(void *), const char * const n
 // #include <FreeRTOS.h>
 // #include <task.h>
 int csp_freertos_thread_create(TaskFunction_t func, const char * const name, unsigned int stack_size, void * parameter, unsigned int priority, void ** handle);
-
-#elif (CSP_ZEPHYR)
-#include <zephyr.h>
-k_tid_t csp_zephyr_thread_create_static(k_tid_t *new_thread, const char * const name, char *stack, unsigned int stack_size, k_thread_entry_t func, void * parameter, unsigned int priority);
 #endif
 
 /**
--- src/arch/zephyr/csp_thread.c
+++ src/arch/zephyr/csp_thread.c
@@ -1,25 +1,4 @@
-
-
 #include <csp/arch/csp_thread.h>
-
-k_tid_t
-csp_zephyr_thread_create_static(k_tid_t * new_thread,
-						 const char * const thread_name,
-						 char * stack,
-						 unsigned int stack_size,
-						 k_thread_entry_t entry,
-						 void * param,
-						 unsigned int priority) {
-	k_tid_t tid;
-
-	tid = k_thread_create(new_thread,
-						  stack, stack_size,
-						  entry, param, NULL, NULL,
-						  priority, 0, K_NO_WAIT);
-	k_thread_name_set(tid, thread_name);
-
-	return tid;
-}
 
 void csp_sleep_ms(unsigned int time_ms) {
 	k_sleep(K_MSEC(time_ms));
