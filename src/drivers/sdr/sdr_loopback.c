e580f2bf398c68472085fabff535f81517b15578
Ron Unrau <rcunrau@ualberta.ca>
2021-11-05 12:30:24 -0600

    sdr_loopback.c: MPDU size is always MTU, which is not always 128

--- src/drivers/sdr/sdr_loopback.c
+++ src/drivers/sdr/sdr_loopback.c
@@ -12,7 +12,7 @@
 static int sdr_loopback_tx(void *iface, const uint8_t *data, size_t len) {
     csp_sdr_interface_data_t *ifdata = ((csp_iface_t *) iface)->interface_data;
 
-    /* The UHF payload is always 128 bytes */
+    /* The UHF payload is always MTU bytes */
     if (xQueueSend(ifdata->rx_queue, data, QUEUE_NO_WAIT) != pdPASS) {
         return CSP_ERR_NOBUFS;
     }
@@ -23,6 +23,7 @@ static int sdr_loopback_tx(void *iface, const uint8_t *data, size_t len) {
 void* sdr_loopback_open(csp_iface_t *iface) {
     csp_sdr_interface_data_t *ifdata = iface->interface_data;
 
+    iface->split_horizon_off = 0;
     ifdata->tx_mac = sdr_loopback_tx;
     return iface;
 }
