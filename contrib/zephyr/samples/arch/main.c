39bcbc7a4be92077f9bae579e9a7b2c7a7c87e57
Yasushi SHOJI <yashi@spacecubics.com>
2021-11-13 08:48:29 +0900

    csp_thread: Fix the comment in samples/arch/main.c
    csp_thread is now gone.  Remove the comment.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- contrib/zephyr/samples/arch/main.c
+++ contrib/zephyr/samples/arch/main.c
@@ -41,7 +41,7 @@ int main(int argc, char * argv[]) {
     csp_log_protocol("csp_log_protocol(...), level: %d", CSP_PROTOCOL);
     csp_log_lock("csp_log_lock(...), level: %d", CSP_LOCK);
 
-    // create a thread - csp_thread doesn't support join
+    // create a thread
     k_tid_t tid;
     struct k_thread new_thread;
 
