70e43adec06dea20dc2c484ca37fb66254f83267
Johan de Claville Christiansen <johan@space-inventor.com>
2021-11-18 12:29:06 +0100

    new: csp_if_tun: New IPsec like tunnel interface for secured links

---
+++ include/csp/interfaces/csp_if_tun.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <csp/csp.h>
+
+typedef struct {
+	/* Should be set before calling if_tun_init */
+	int tun_src;
+	int tun_dst;
+} csp_if_tun_conf_t;
+
+void csp_if_tun_init(csp_iface_t * iface, csp_if_tun_conf_t * ifconf);
---
+++ src/interfaces/csp_if_tun.c
@@ -0,0 +1,122 @@
+#include <csp/interfaces/csp_if_tun.h>
+#include <csp/csp.h>
+#include <csp/csp_id.h>
+
+/** Implement these, if you use csp_if_tun */
+int csp_crypto_decrypt(uint8_t * ciphertext_in, uint8_t ciphertext_len, uint8_t * msg_out); // Returns -1 for failure, length if ok
+int csp_crypto_encrypt(uint8_t * msg_begin, uint8_t msg_len, uint8_t * ciphertext_out); // Returns length of encrypted data
+
+static int csp_if_tun_tx(const csp_route_t * ifroute, csp_packet_t * packet) {
+
+	csp_if_tun_conf_t * ifconf = ifroute->iface->driver_data;
+
+	/* Allocate new frame */
+	csp_packet_t * new_packet = csp_buffer_get(packet->frame_length);
+	if (new_packet == NULL) {
+		csp_buffer_free(packet);
+		return CSP_ERR_NONE;
+	}
+
+	if (packet->id.dst == ifconf->tun_src) {
+
+		/**
+		 * Incomming tunnel packet
+		 */
+		//csp_hex_dump("incoming packet", packet->data, packet->length);
+
+		csp_id_setup_rx(new_packet);
+
+#if 1
+		int length = csp_crypto_decrypt(packet->data, packet->length, new_packet->frame_begin);
+		if (length < 0) {
+			csp_buffer_free(new_packet);
+			csp_buffer_free(packet);
+			ifroute->iface->rx_error++;
+			printf("Decryption error\n");
+			return CSP_ERR_NONE;
+		} else {
+			new_packet->frame_length = length;
+		}
+#else
+		/* Decapsulate */
+		memcpy(new_packet->frame_begin, packet->data, packet->length);
+		new_packet->frame_length = packet->length;
+#endif
+
+		/* Now free old packet */
+		csp_buffer_free(packet);
+
+		//csp_hex_dump("new frame", new_packet->frame_begin, new_packet->frame_length + 16);
+
+		csp_id_strip(new_packet);
+
+		//csp_hex_dump("new packet", new_packet->data, new_packet->length);
+
+		/* Send new packet */
+		csp_qfifo_write(new_packet, ifroute->iface, NULL);
+
+	} else {
+
+		/**
+		 * Outgoing tunnel packet
+		 */
+
+		//csp_hex_dump("packet", packet->data, packet->length);
+
+		/* Apply CSP header */
+		csp_id_prepend(packet);
+
+		//csp_hex_dump("frame", packet->frame_begin, packet->frame_length);
+
+		/* Create tunnel header */
+		new_packet->id.dst = ifconf->tun_dst;
+		new_packet->id.src = ifconf->tun_src;
+		new_packet->id.sport = 0;
+		new_packet->id.dport = 0;
+		new_packet->id.pri = packet->id.pri;
+		new_packet->length = packet->frame_length;
+
+#if 1
+		/* Encrypt */
+		new_packet->length = csp_crypto_encrypt(packet->frame_begin, packet->frame_length, new_packet->data);
+#else
+		/* Encapsulate */
+		memcpy(new_packet->data, packet->frame_begin, packet->frame_length);
+#endif
+
+		/* Free old packet */
+		csp_buffer_free(packet);
+
+		//csp_hex_dump("new packet", new_packet->data, new_packet->length);
+
+		/* Apply CSP header */
+		csp_id_prepend(new_packet);
+
+		//csp_hex_dump("new frame", new_packet->frame_begin, new_packet->frame_length);
+
+		/* Send new packet */
+		csp_qfifo_write(new_packet, ifroute->iface, NULL);
+
+	}
+
+	return CSP_ERR_NONE;
+
+}
+
+
+void csp_if_tun_init(csp_iface_t * iface, csp_if_tun_conf_t * ifconf) {
+
+	iface->driver_data = ifconf;
+
+	//printf("Setup Tunnel between %d and %d\n", ifconf->tun_src, ifconf->tun_dst);
+
+	/* MTU is datasize */
+	iface->mtu = csp_buffer_data_size();
+
+	/* Regsiter interface */
+	iface->name = "TUN",
+	iface->nexthop = csp_if_tun_tx,
+	csp_iflist_add(iface);
+
+}
+
