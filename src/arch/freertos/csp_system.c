f7536eeaf2648741576bc822c8c9eb6ee1b0dc52
Johan de Claville Christiansen <johan@space-inventor.com>
2021-12-05 19:19:10 +0100

    Provide empty csp_memfree_hook for freertos when:
    dynamic allocation is not enabled

--- src/arch/freertos/csp_system.c
+++ src/arch/freertos/csp_system.c
@@ -3,8 +3,10 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
+__attribute__((weak)) uint32_t csp_memfree_hook(void) {
 #if (configSUPPORT_DYNAMIC_ALLOCATION == 1)
-uint32_t csp_memfree_hook(void) {
 	return (uint32_t)xPortGetFreeHeapSize();
-}
+#else
+	return 0;
 #endif
+}
