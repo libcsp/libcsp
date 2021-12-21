40373bf93e90ab4d372d149aad334dfe238c1eed
Yasushi SHOJI <yashi@spacecubics.com>
2021-12-21 16:24:32 +0900

    zephyr: Remove debug print hook function
    Debug print facility has been removed by the commit 39d6478;
    csp_debug_hook_set(), csp_debug_level_t, CSP_ERROR and its friends are
    gone.
    
    Remove it from Zephyr as well.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/arch/zephyr/csp_zephyr_init.c
+++ src/arch/zephyr/csp_zephyr_init.c
@@ -8,24 +8,9 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(libcsp);
 
-static void hook_func(csp_debug_level_t level, const char * format, va_list args) {
-	uint32_t args_num = log_count_args(format);
-
-	switch (level) {
-		case CSP_ERROR:
-			Z_LOG_VA(LOG_LEVEL_ERR, format, args, args_num, LOG_STRDUP_EXEC);
-			break;
-		case CSP_WARN:
-			Z_LOG_VA(LOG_LEVEL_WRN, format, args, args_num, LOG_STRDUP_EXEC);
-			break;
-		default:
-			Z_LOG_VA(LOG_LEVEL_INF, format, args, args_num, LOG_STRDUP_EXEC);
-			break;
-	}
 }
 
 static int libcsp_zephyr_init(const struct device * unused) {
-	csp_debug_hook_set(hook_func);
 
 	struct timespec ts = {
 		.tv_sec = 946652400,
