2cf2edfdafad53f61e6720fc3dfcb9f945d45daf
Johan de Claville Christiansen <johan@space-inventor.com>
2022-03-09 16:02:37 +0100

    Print debug

--- src/csp_io.c
+++ src/csp_io.c
@@ -78,7 +78,7 @@ void csp_id_copy(csp_id_t * target, csp_id_t * source) {
 	target->sport = source->sport;
 	target->flags = source->flags;
 }
-
+#include <stdio.h>
 void csp_send_direct(csp_id_t idout, csp_packet_t * packet, csp_iface_t * routed_from) {
 
 	int from_me = (routed_from == NULL ? 1 : 0);
@@ -113,6 +113,7 @@ void csp_send_direct(csp_id_t idout, csp_packet_t * packet, csp_iface_t * routed
 		copy = csp_buffer_clone(packet);
 		csp_send_direct_iface(idout, copy, iface, via, from_me);
 
+		printf("To local %s\n", iface->name);
 		local_found = 1;
 
 	}
@@ -140,6 +141,7 @@ void csp_send_direct(csp_id_t idout, csp_packet_t * packet, csp_iface_t * routed
 		}
 
 		csp_send_direct_iface(idout, packet, route->iface, route->via, from_me);
+		printf("To rtable %s\n", route->iface->name);
 		return;
 	}
 
@@ -160,6 +162,7 @@ void csp_send_direct(csp_id_t idout, csp_packet_t * packet, csp_iface_t * routed
 		}
 
 		csp_send_direct_iface(idout, packet, dfl_if, CSP_NO_VIA_ADDRESS, from_me);
+		printf("To dfl if %s\n", dfl_if->name);
 		return;
 	}
 
