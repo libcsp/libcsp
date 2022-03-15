ff87f9f8b28d24784b92ca710b8fe03bb63acaa5
Robert Taylor <taylorr82@mymacewan.ca>
2022-03-14 23:29:52 -0600

    Fix ambiguous variable name

--- src/drivers/sdr/fec.c
+++ src/drivers/sdr/fec.c
@@ -10,8 +10,11 @@
 #include "radio.h"
 #include <error_correctionWrapper.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-static mac_t mac;
+static mac_t *my_mac;
 
 // each packet starts like this
 struct mpdu {
@@ -25,42 +28,46 @@ struct mpdu {
 static uint32_t csp_index;
 
 bool fec_csp_to_mpdu(CircularBufferHandle fifo, csp_packet_t *packet, int mtu) {
-    return receive_csp_packet(mac, packet);
+    return receive_csp_packet(my_mac, packet);
 }
 
 bool fec_mpdu_to_csp(const void *buf, csp_packet_t **packet, uint8_t *id, int mtu) {
-    if (process_uhf_packet(mac, buf, mtu) == CSP_PACKET_READY) {
-        const uint8_t *rawCSP = get_raw_csp_packet_buffer(mac);
-        const uint32_t rawLen = get_raw_csp_packet_length(mac);
-        uint8_t *newpacket = csp_malloc(rawLen);
+    if (process_uhf_packet(my_mac, (const uint8_t *)buf, mtu) == CSP_PACKET_READY) {
+        const uint8_t *rawCSP = get_raw_csp_packet_buffer(my_mac);
+        const uint32_t rawLen = get_raw_csp_packet_buffer_length(my_mac);
+        //uint8_t *newpacket = csp_malloc(rawLen);
 
-        memcpy(newpacket, rawCSP, rawLen);
+        //memcpy(newpacket, rawCSP, rawLen);
 
-        *packet = (csp_packet_t *)newpacket;
+        *packet = csp_buffer_clone(rawCSP);//(csp_packet_t *)newpacket;
         return true;
     }
     return false;
 }
 
 void fec_create(rf_mode_number_t rfmode, error_correction_scheme_t error_correction_scheme) {
-    mac = mac_create(rfmode, error_correction_scheme);
+    my_mac = mac_create(rfmode, error_correction_scheme);
 }
 
 // Returns 0 if none exist, else returns mpdu size
 int fec_get_next_mpdu(void **buf) {
     static int index = 0;
-    const uint32_t length = mpdu_payloads_buffer_length(mac);
+    const uint32_t length = mpdu_payloads_buffer_length(my_mac);
     if (index >= length) {
         index = 0;
         return 0;
     }
-    const uint8_t *mpdusBuffer = mpdu_payloads_buffer(mac);
+    const uint8_t *mpdusBuffer = mpdu_payloads_buffer(my_mac);
     const uint32_t mtu = raw_mpdu_length();
-    *buf = mpdusBuffer[index*mtu];
-    index++;
+    *buf = mpdusBuffer + index;
+    index += mtu;
     return mtu;
 }
 
 int fec_get_mtu() {
     return raw_mpdu_length();
 }
+
+#ifdef __cplusplus
+}
+#endif
