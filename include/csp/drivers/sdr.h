0be9cbcd2d41be4a96dbe5d9539bf4ededc141ac
Ron Unrau <runrau@Rons-MacBook-Pro.local>
2022-06-03 12:05:19 -0600

    Most of the include files moved to ex2_sdr/include/driver, but the CSP specific stuff stays here.

--- include/csp/drivers/sdr.h
+++ include/csp/drivers/sdr.h
@@ -1,32 +1,18 @@
 #ifndef CSP_DRIVERS_SDR_H
 #define CSP_DRIVERS_SDR_H
 
+#include <csp/csp.h>
+#include "sdr_driver.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef enum {
-    SDR_UHF_1200_BAUD = 0,
-    SDR_UHF_2400_BAUD,
-    SDR_UHF_4800_BAUD,
-    SDR_UHF_9600_BAUD,
-    SDR_UHF_19200_BAUD,
-    SDR_UHF_TEST_BAUD,
-    SDR_UHF_END_BAUD,
-} sdr_uhf_baud_rate_t;
-
-typedef struct csp_sdr_conf {
-    uint16_t mtu;
-    sdr_uhf_baud_rate_t baudrate;
-    int uart_baudrate;
-    char *device_file;
-} csp_sdr_conf_t;
 
 int csp_sdr_tx(const csp_route_t *ifroute, csp_packet_t *packet);
 
 void csp_sdr_rx(void *cb_data, void *buf, size_t len, void *pxTaskWoken);
 
-int csp_sdr_driver_init(csp_iface_t * iface);
+int csp_sdr_uart_driver_init(sdr_uhf_conf_t *sdr_conf);
 
 #ifdef __cplusplus
 }
