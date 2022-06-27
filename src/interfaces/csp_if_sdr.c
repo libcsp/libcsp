ab0cd78ceb5d27e0bee5119c1f4209d9cf80084d
Ron Unrau <rcunrau@ualberta.ca>
2022-06-27 16:00:23 -0600

    csp_if_sdr.c: move ifdata->iface to sdr_conf->rx_callback_data

--- src/interfaces/csp_if_sdr.c
+++ src/interfaces/csp_if_sdr.c
@@ -9,26 +9,27 @@
 #include <csp/csp_endian.h>
 #include <sdr_driver.h>
 
+
 int csp_if_sdr_tx(const csp_route_t *ifroute, csp_packet_t *packet) {
     sdr_interface_data_t *ifdata = (sdr_interface_data_t *)ifroute->iface->interface_data;
     uint16_t len = packet->length;
 	packet->id.ext = csp_hton32(packet->id.ext);
 	packet->length = csp_hton16(len);
 
-    if (sdr_uhf_tx(ifdata, (uint8_t *)packet, len) == 0) {
-        return CSP_ERR_NONE;
-    } else {
+    if (sdr_uhf_tx(ifdata, (uint8_t *)packet, len + sizeof(csp_packet_t))) {
         return CSP_ERR_TX;
     }
+    return CSP_ERR_NONE;
 }
 
 void csp_if_sdr_rx(void *udata, uint8_t *data, size_t len) {
-    sdr_interface_data_t *ifdata = (sdr_interface_data_t *)udata;
-    csp_iface_t *iface = ifdata->iface;
     csp_packet_t *packet = (csp_packet_t *)data;
     packet->length = csp_ntoh16(packet->length);
     packet->id.ext = csp_ntoh32(packet->id.ext);
+
     csp_packet_t *clone = csp_buffer_clone(packet);
+
+    csp_iface_t *iface = (csp_iface_t *) udata;
     csp_qfifo_write(clone, iface, NULL);
 }
 
@@ -52,11 +53,10 @@ int csp_uhf_open_and_add_interface(const sdr_uhf_conf_t *conf, const char *ifnam
 
     sdr_uhf_conf_t *sdr_conf = ifdata->sdr_conf;
     sdr_conf->rx_callback = csp_if_sdr_rx;
+    sdr_conf->rx_callback_data = iface;
 
     iface->nexthop = csp_if_sdr_tx;
     csp_iflist_add(iface);
-
-    ifdata->iface = (void *)iface;
 
     if (return_iface) {
         *return_iface = iface;
