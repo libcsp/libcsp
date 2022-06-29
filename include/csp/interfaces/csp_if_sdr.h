9ff59af52d10302af69abacda3b65b8dd3b13dd3
joshdellaz <josh.lazaruk@gmail.com>
2022-06-29 10:50:40 -0700

    move gnuradio to ex2_sdr

--- include/csp/interfaces/csp_if_sdr.h
+++ include/csp/interfaces/csp_if_sdr.h
@@ -5,7 +5,5 @@
 #include <sdr_driver.h>
 
 int csp_uhf_open_and_add_interface(const sdr_uhf_conf_t *conf, const char *ifname, csp_iface_t **return_iface);
-int csp_if_sdr_tx(const csp_route_t *ifroute, csp_packet_t *packet);
-void csp_if_sdr_rx(void *udata, uint8_t *data, size_t len);
 
 #endif /* CSP_INTERFACES_CSP_IF_SDR_H */
