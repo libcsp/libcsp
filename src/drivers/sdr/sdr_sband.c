8b3474c57e9b976eb3b9cdf14a441efc9b3b76c1
Ron Unrau <rcunrau@ualberta.ca>
2022-07-01 14:17:47 -0600

    sdr_sband.c: add a low level tx function

--- src/drivers/sdr/sdr_sband.c
+++ src/drivers/sdr/sdr_sband.c
@@ -20,10 +20,28 @@
 #include <string.h>
 #include <csp/csp.h>
 #include <sdr_driver.h>
+#include <spi.h>
+#include <logger/logger.h>
+
+static int sband_tx(int fd, const void *data, size_t len) {
+#ifdef SBAND_IS_STUBBED
+    char buf[96];
+    char *msg = buf;
+    const uint8_t *ptr = data;
+    for (int i=0; i<len && i<32; i++) {
+        sprintf(msg, "%02x ", ptr[i]);
+        msg += 3;
+    }
+    ex2_log(buf);
+#else
+    SPISbandTx(data, len);
+#endif
+    return 0;
+}
 
 int sdr_sband_driver_init(sdr_interface_data_t *ifdata) {
     ifdata->fd = (uintptr_t) 0;
-    ifdata->tx_func = (sdr_tx_t) 0;
+    ifdata->tx_func = sband_tx;
 
-    return CSP_ERR_NONE;
+    return 0;
 }
