5acc7ac42c2764271b3079c7753cd6daa73d8833
Johan de Claville Christiansen <johan@space-inventor.com>
2022-11-09 14:57:37 +0100

    Fix buffer leak in csp_buffer_free_isr()

--- src/csp_buffer.c
+++ src/csp_buffer.c
@@ -5,18 +5,12 @@
 #include <csp/arch/csp_queue.h>
 #include <csp/csp_debug.h>
 
-#ifndef CSP_BUFFER_ALIGN
-#define CSP_BUFFER_ALIGN (sizeof(int *))
-#endif
-
 /** Internal buffer header */
 typedef struct csp_skbf_s {
 	unsigned int refcount;
 	void * skbf_addr;
-	char skbf_data[sizeof(csp_packet_t)];
+	csp_packet_t skbf_data;
 } csp_skbf_t;
-
-#define SKBUF_SIZE CSP_BUFFER_ALIGN * ((sizeof(csp_skbf_t)+ (CSP_BUFFER_ALIGN - 1)) / CSP_BUFFER_ALIGN)
 
 // Queue of free CSP buffers
 static csp_queue_handle_t csp_buffers;
@@ -26,16 +20,16 @@ void csp_buffer_init(void) {
 	 * Chunk of memory allocated for CSP buffers:
 	 * This is marked as .noinit, because csp buffers can never be assumed zeroed out
 	 * Putting this section in a separate non .bss area, saves some boot time */
-	static char csp_buffer_pool[SKBUF_SIZE * CSP_BUFFER_COUNT] __attribute__((section(".noinit")));
+	static csp_skbf_t csp_buffer_pool[CSP_BUFFER_COUNT]  __attribute__((section(".noinit")));
 	static csp_static_queue_t csp_buffers_queue __attribute__((section(".noinit")));
 	static char csp_buffer_queue_data[CSP_BUFFER_COUNT * sizeof(csp_skbf_t *)] __attribute__((section(".noinit")));
 
 	csp_buffers = csp_queue_create_static(CSP_BUFFER_COUNT, sizeof(csp_skbf_t *), csp_buffer_queue_data, &csp_buffers_queue);
 
 	for (unsigned int i = 0; i < CSP_BUFFER_COUNT; i++) {
-		csp_skbf_t * buf = (void *)&csp_buffer_pool[i * SKBUF_SIZE];
-		buf->skbf_addr = buf;
-		csp_queue_enqueue(csp_buffers, &buf, 0);
+		csp_buffer_pool[i].skbf_addr = &csp_buffer_pool[i];
+		csp_skbf_t * bufptr = &csp_buffer_pool[i];
+		csp_queue_enqueue(csp_buffers, &bufptr, 0);
 	}
 }
 
@@ -59,7 +53,7 @@ void * csp_buffer_get_isr(size_t _data_size) {
 	}
 
 	buffer->refcount = 1;
-	return buffer->skbf_data;
+	return &buffer->skbf_data;
 }
 
 void * csp_buffer_get(size_t _data_size) {
@@ -82,7 +76,7 @@ void * csp_buffer_get(size_t _data_size) {
 	}
 
 	buffer->refcount = 1;
-	return buffer->skbf_data;
+	return &buffer->skbf_data;
 }
 
 void csp_buffer_free_isr(void * packet) {
@@ -92,12 +86,7 @@ void csp_buffer_free_isr(void * packet) {
 		return;
 	}
 
-	csp_skbf_t * buf = (void *)(((uint8_t *)packet) - sizeof(csp_skbf_t));
-
-	if (((uintptr_t)buf % CSP_BUFFER_ALIGN) > 0) {
-		csp_dbg_errno = CSP_DBG_ERR_CORRUPT_BUFFER;
-		return;
-	}
+	csp_skbf_t * buf = packet - offsetof(csp_skbf_t, skbf_data);
 
 	if (buf->skbf_addr != buf) {
 		csp_dbg_errno = CSP_DBG_ERR_CORRUPT_BUFFER;
@@ -105,12 +94,12 @@ void csp_buffer_free_isr(void * packet) {
 	}
 
 	if (buf->refcount == 0) {
-		csp_dbg_errno = CSP_DBG_ERR_CORRUPT_BUFFER;
+		csp_dbg_errno = CSP_DBG_ERR_ALREADY_FREE;
 		return;
 	}
 
 	if (--(buf->refcount) > 0) {
-		csp_dbg_errno = CSP_DBG_ERR_CORRUPT_BUFFER;;
+		csp_dbg_errno = CSP_DBG_ERR_REFCOUNT;;
 		return;
 	}
 
@@ -125,12 +114,7 @@ void csp_buffer_free(void * packet) {
 		return;
 	}
 
-	csp_skbf_t * buf = (void *)(((uint8_t *)packet) + sizeof(csp_packet_t) - sizeof(csp_skbf_t));
-
-	if (((uintptr_t)buf % CSP_BUFFER_ALIGN) > 0) {
-		csp_dbg_errno = CSP_DBG_ERR_CORRUPT_BUFFER;
-		return;
-	}
+	csp_skbf_t * buf = packet - offsetof(csp_skbf_t, skbf_data);
 
 	if (buf->skbf_addr != buf) {
 		csp_dbg_errno = CSP_DBG_ERR_CORRUPT_BUFFER;
