fba98a71fa5b29e6ed20df8d5b51635f387a82d5
Abestanis <abestanis.gc@gmail.com>
2023-11-14 02:00:56 +0100

    Fix possible endless loop when routing
    There are two conditions in this loop where we can continue,
    but in these cases route is never changed
    so we were just looping endlessly.

--- src/csp_io.c
+++ src/csp_io.c
@@ -114,7 +114,7 @@ void csp_send_direct(csp_id_t* idout, csp_packet_t * packet, csp_iface_t * route
 		if (csp_id_is_broadcast(idout->dst, iface)) {
 			idout->dst = csp_id_get_max_nodeid();
 		}
-		
+
 		/* Todo: Find an elegant way to avoid making a copy when only a single destination interface
 		 * is found. But without looping the list twice. And without using stack memory.
 		 * Is this even possible? */
@@ -135,33 +135,33 @@ void csp_send_direct(csp_id_t* idout, csp_packet_t * packet, csp_iface_t * route
 	/* Try to send via routing table */
 	int route_found = 0;
 	csp_route_t * route = csp_rtable_find_route(idout->dst);
-	while (route != NULL) {
+	if (route != NULL) {
+		do {
+			route_found = 1;
 
-		route_found = 1;
+			/* Do not send back to same inteface (split horizon)
+			* This check is is similar to that below, but faster */
+			if (route->iface == routed_from) {
+				continue;
+			}
 
-		/* Do not send back to same inteface (split horizon)
-		 * This check is is similar to that below, but faster */
-		if (route->iface == routed_from) {
-			continue;
-		}
+			/* Do not send to interface with similar subnet (split horizon) */
+			if (csp_iflist_is_within_subnet(route->iface->addr, routed_from)) {
+				continue;
+			}
 
-		/* Do not send to interface with similar subnet (split horizon) */
-		if (csp_iflist_is_within_subnet(route->iface->addr, routed_from)) {
-			continue;
-		}
+			/* Apply outgoing interface address to packet */
+			if ((from_me) && (idout->src == 0)) {
+				idout->src = route->iface->addr;
+			}
 
-		/* Apply outgoing interface address to packet */
-		if ((from_me) && (idout->src == 0)) {
-			idout->src = route->iface->addr;
-		}
-
-		copy = csp_buffer_clone(packet);
-		if (copy != NULL) {
-			csp_send_direct_iface(idout, copy, route->iface, route->via, from_me);
-		}
-
-		route = csp_rtable_search_backward(route);
+			copy = csp_buffer_clone(packet);
+			if (copy != NULL) {
+				csp_send_direct_iface(idout, copy, route->iface, route->via, from_me);
+			}
+		} while ((route = csp_rtable_search_backward(route)) != NULL);
 	}
+
 	/* If the above worked, we don't want to look at default interfaces */
 	if (route_found == 1) {
 		csp_buffer_free(packet);
@@ -173,7 +173,7 @@ void csp_send_direct(csp_id_t* idout, csp_packet_t * packet, csp_iface_t * route
 	/* Try to send via default interfaces */
 	while ((iface = csp_iflist_get_by_isdfl(iface)) != NULL) {
 
-		/* Do not send back to same inteface (split horizon) 
+		/* Do not send back to same inteface (split horizon)
 		 * This check is is similar to that below, but faster */
 		if (iface == routed_from) {
 			continue;
