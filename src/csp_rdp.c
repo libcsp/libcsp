7a327629558f783f84f6a28a83307564ab7379bb
Yasushi SHOJI <yashi@spacecubics.com>
2024-01-30 22:37:05 +0900

    csp_rdp: Define _DEFAULT_SOURCE for rand_r()
    rand_r() is a function POSIX 2001 (and deprecated in 2008).  If you
    compile with -std=c11 or without GNU extension (__STRICT_ANSI__),
    rand_r() is not declared.
    
    With _DEFAULT_SOURCE, we can have POSIX functions as well as functions
    from BSD and SVID.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/csp_rdp.c
+++ src/csp_rdp.c
@@ -1,3 +1,5 @@
+#define _DEFAULT_SOURCE
+
 /*
  * This is a implementation of the seq/ack handling taken from the Reliable Datagram Protocol (RDP)
  * For more information read RFC 908/1151. The implementation has been extended to include support for
