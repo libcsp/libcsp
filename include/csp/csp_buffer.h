e68b0e5666730ebf26cfe5e75748fe26f4323160
andrian.dimitrov <andrian.dimitrov94@gmail.com>
2024-02-19 11:08:49 +0200

    Run clang-format

--- include/csp/csp_buffer.h
+++ include/csp/csp_buffer.h
@@ -32,14 +32,14 @@ csp_packet_t * csp_buffer_get_isr(size_t unused);
  *
  * @param[in] buffer buffer to free. NULL is handled gracefully.
  */
-void csp_buffer_free(void *buffer);
+void csp_buffer_free(void * buffer);
 
 /**
  * Free buffer (from ISR context).
  *
  * @param[in] buffer buffer to free. NULL is handled gracefully.
  */
-void csp_buffer_free_isr(void *buffer);
+void csp_buffer_free_isr(void * buffer);
 
 /**
  * Clone an existing buffer.
@@ -48,7 +48,7 @@ void csp_buffer_free_isr(void *buffer);
  * @param[in] buffer buffer to clone.
  * @return cloned buffer on success, or NULL on failure.
  */
-void * csp_buffer_clone(void *buffer);
+void * csp_buffer_clone(void * buffer);
 
 /**
  * Return number of remaining/free buffers.
@@ -69,9 +69,7 @@ void csp_buffer_init(void);
 size_t csp_buffer_data_size(void);
 
 void csp_buffer_refc_inc(void * buffer);
-  
+
 #ifdef __cplusplus
 }
 #endif
-
-
