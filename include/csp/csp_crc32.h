5ef9640bdd5393a5941709c0f2b5dc21aaf5624f
Yasushi SHOJI <yashi@spacecubics.com>
2024-02-26 23:37:25 +0900

    include: csp_crc32: Fix @return typo
    @preturn typo was introduced by the commit 92f0e1008b0931057. Fix
    them.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- include/csp/csp_crc32.h
+++ include/csp/csp_crc32.h
@@ -23,7 +23,7 @@ typedef uint32_t csp_crc32_t;
  * Append CRC32 checksum to packet
  *
  * @param[in] packet CSP packet, must be valid.
- * @preturn #CSP_ERR_NONE on success, otherwise an error code.
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_crc32_append(csp_packet_t * packet);
 
@@ -31,7 +31,7 @@ int csp_crc32_append(csp_packet_t * packet);
  * Verify CRC32 checksum on packet.
  *
  * @param[in] packet CSP packet, must be valid.
- * @preturn #CSP_ERR_NONE on success, otherwise an error code.
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_crc32_verify(csp_packet_t * packet);
 

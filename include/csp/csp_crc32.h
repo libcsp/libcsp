libcsp/libcsp

commit 92f0e1008b093105712832de4f27745d07088128

include: csp: Change header files docstrings
Use '@param[]' & '@return' across all headers for function's arguments and
return types

Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

include/csp/csp_crc32.h
@@ -14,34 +14,25 @@ extern "C" {
 /**
  * Append CRC32 checksum to packet
  *
- * Parameters:
- *	packet (csp_packet_t *) [in]: CSP packet, must be valid.
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] packet CSP packet, must be valid.
+ * @preturn #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_crc32_append(csp_packet_t * packet);
 
 /**
  * Verify CRC32 checksum on packet.
  *
- * Parameters:
- *	packet (csp_packet_t *) [in]: CSP packet, must be valid.
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] packet CSP packet, must be valid.
+ * @preturn #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_crc32_verify(csp_packet_t * packet);
 
 /**
  * Calculate checksum for a given memory area.
  *
- * Parameters:
- *	addr (const uint8_t *) [in]: memory address
- *	length (uint32_t) [in]: length of memory to do checksum on
- *
- * Returns:
- *	uint32_t: checksum
+ * @param[in] addr memory address
+ * @param[in] length length of memory to do checksum on
+ * @return checksum
  */
 uint32_t csp_crc32_memory(const uint8_t * addr, uint32_t length);
 

include/csp/arch/csp_queue.h
@@ -37,83 +37,63 @@ csp_queue_handle_t csp_queue_create_static(int length, size_t item_size, char * 
 /**
  * Enqueue (back) value.
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: queue.
- *	value (const void *) [in]: value to add (by copy)
- *	timeout (uint32_t) [in]: timeout, time to wait for free space
- *
- * Returns:
- *	int: #CSP_QUEUE_OK on success, otherwise a queue error code.
+ * @param[in] handle queue.
+ * @param[in] value value to add (by copy)
+ * @param[in] timeout timeout, time to wait for free space
+ * @return #CSP_QUEUE_OK on success, otherwise a queue error code.
  */
 int csp_queue_enqueue(csp_queue_handle_t handle, const void *value, uint32_t timeout);
 
 /**
  * Enqueue (back) value from ISR.
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: queue.
- *	value (csp_queue_handle_t) [in]: value to add (by copy)
- *	pxTaskWoken (int *) [out]: Valid reference if called from ISR, otherwise NULL!
- *
- * Returns:
- *	int: #CSP_QUEUE_OK on success, otherwise a queue error code.
+ * @param[in] handle queue.
+ * @param[in] value value to add (by copy)
+ * @param[out] pxTaskWoken Valid reference if called from ISR, otherwise NULL!
+ * @return #CSP_QUEUE_OK on success, otherwise a queue error code.
  */
 int csp_queue_enqueue_isr(csp_queue_handle_t handle, const void * value, int * pxTaskWoken);
 
 /**
  * Dequeue value (front).
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: queue.
- *	buf (void *) [out]: extracted element (by copy).
- *	timeout (uint32_t) [in]: timeout, time to wait for element in queue.
- *
- * Returns:
- *	int: #CSP_QUEUE_OK on success, otherwise a queue error code.
+ * @param[in] handle queue.
+ * @param[out] buf extracted element (by copy).
+ * @param[in] timeout timeout, time to wait for element in queue.
+ * @return #CSP_QUEUE_OK on success, otherwise a queue error code.
  */
 int csp_queue_dequeue(csp_queue_handle_t handle, void *buf, uint32_t timeout);
 
 /**
  * Dequeue value (front) from ISR.
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: queue.
- *	buf (void *) [out]: extracted element (by copy).
- *	pxTaskWoken (int *) [out]: Valid reference if called from ISR, otherwise NULL!
- *
- * Returns:
- *	int: #CSP_QUEUE_OK on success, otherwise a queue error code.
+ * @param[in] handle queue.
+ * @param[out] buf extracted element (by copy).
+ * @param[out] pxTaskWoken Valid reference if called from ISR, otherwise NULL!
+ * @return #CSP_QUEUE_OK on success, otherwise a queue error code.
  */
 int csp_queue_dequeue_isr(csp_queue_handle_t handle, void * buf, int * pxTaskWoken);
 
 /**
  * Queue size.
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: handle queue.
- *
- * Returns:
- *	int: Number of elements in the queue.
+ * @param[in] handle handle queue.
+ * @return Number of elements in the queue.
  */
 int csp_queue_size(csp_queue_handle_t handle);
 
 /**
  * Queue size from ISR.
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: handle queue.
- *
- * Returns:
- *	int: Number of elements in the queue.
+ * @param[in] handle handle queue.
+ * @return Number of elements in the queue.
  */
 int csp_queue_size_isr(csp_queue_handle_t handle);
 
 /**
  * Free queue object (handle).
  *
- * Parameters:
- *	handle (csp_queue_handle_t) [in]: handle queue.
- *
+ * @param[in] handle handle queue.
  */
 int csp_queue_free(csp_queue_handle_t handle);
 

include/csp/crypto/csp_hmac.h
@@ -22,24 +22,18 @@ extern "C" {
  * Append HMAC to packet
  * If header is included, csp_id_prepend() must be called beforehand
  *
- * Parameters:
- *	packet (csp_packet_t *) [in]: CSP packet, must be valid.
- *	include_header (bool) [in]: use header in hmac calculation (this will not modify the flags field)
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] packet CSP packet, must be valid.
+ * @param[in] include_header use header in hmac calculation (this will not modify the flags field)
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_hmac_append(csp_packet_t * packet, bool include_header);
 
 /**
  * Verify HMAC of packet
  *
- * Parameters:
- *	packet (csp_packet_t *) [in]: CSP packet, must be valid.
- *	include_header (bool) [in]: use header in hmac calculation (this will not modify the flags field)
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] packet CSP packet, must be valid.
+ * @param[in] include_header use header in hmac calculation (this will not modify the flags field)
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_hmac_verify(csp_packet_t * packet, bool include_header);
 
@@ -47,27 +41,21 @@ int csp_hmac_verify(csp_packet_t * packet, bool include_header);
  * Calculate HMAC on buffer
  * This function is used by append/verify but cal also be called separately.
  *
- * Parameters:
- *	key (const void *) [in]: HMAC key
- *	keylen (uint32_t) [in]: HMAC key length
- *	data (const void *) [in]: pointer to data
- *	datalen (uint32_t) [in]: lehgth of data
- *	hmac (uint8_t *) [out]: calculated HMAC hash, minimum #CSP_SHA1_DIGESTSIZE bytes.
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] key HMAC key
+ * @param[in] keylen HMAC key length
+ * @param[in] data pointer to data
+ * @param[in] datalen lehgth of data
+ * @param[out] hmac calculated HMAC hash, minimum #CSP_SHA1_DIGESTSIZE bytes.
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_hmac_memory(const void * key, uint32_t keylen, const void * data, uint32_t datalen, uint8_t * hmac);
 
 /**
  * Save a copy of the key string for use by the append/verify functions
  *
- * Parameters:
- *	key (const void *) [in]: HMAC key
- *	keylen (uint32_t) [in]: HMAC key length
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] key HMAC key
+ * @param[in] keylen HMAC key length
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_hmac_set_key(const void * key, uint32_t keylen);
 

include/csp/crypto/csp_sha1.h
@@ -32,37 +32,33 @@ typedef struct {
 /**
  * Initialize the hash state
  *
- * Parameters:
- *	state (csp_sha1_state_t *) [in]: hash state.
+ * @param[in] state hash state.
  */
 void csp_sha1_init(csp_sha1_state_t * state);
 
 /**
  * Process a block of memory through the hash.
  *
- * Parameters:
- *	state (csp_sha1_state_t *) [in]: hash state
- *	data (const void *) [in]: data.
- *	length (uint32_t) [in]: length of data.
+ * @param[in] state hash state
+ * @param[in] data data.
+ * @param[in] length length of data.
  */
 void csp_sha1_process(csp_sha1_state_t * state, const void * data, uint32_t length);
 
 /**
  * Terminate the hash calculation and get the SHA1.
  *
- * Parameters:
- *	state (csp_sha1_state_t *) [in]: hash state
- *	sha1 (uint8_t *) [out]: user supplied buffer of minimum #CSP_SHA1_DIGESTSIZE bytes.
+ * @param[in] state hash state
+ * @param[out] sha1 user supplied buffer of minimum #CSP_SHA1_DIGESTSIZE bytes.
  */
 void csp_sha1_done(csp_sha1_state_t * state, uint8_t * sha1);
 
 /**
  * Calculate SHA1 hash of block of memory.
  *
- * Parameters:
- *	data (const void *) [in]: data.
- *	length (uint32_t) [in]: length of data.
- *	sha1 (uint8_t *) [out]: user supplied buffer of minimum #CSP_SHA1_DIGESTSIZE bytes.
+ * @param[in] data data.
+ * @param[in] length length of data.
+ * @param[out] sha1 user supplied buffer of minimum #CSP_SHA1_DIGESTSIZE bytes.
  */
 void csp_sha1_memory(const void * data, uint32_t length, uint8_t * sha1);
 

include/csp/csp_buffer.h
@@ -12,40 +12,32 @@ extern "C" {
 #endif
 
 /**
- * Get free buffer (from task context).
+ * Get free buffer from task context.
  *
- * Parameters:
- *  data_size (size_t) [in]: OBSOLETE ignored field, csp packets have a fixed size now
- *
- * Returns:
- *  csp_packet_t *: Buffer (pointer to #csp_packet_t) or NULL if no buffers available
+ * @param[in] unused OBSOLETE ignored field, csp packets have a fixed size now
+ * @return Buffer pointer to #csp_packet_t or NULL if no buffers available
  */
 csp_packet_t * csp_buffer_get(size_t unused);
 
 /**
  * Get free buffer (from ISR context).
  *
- * Parameters:
- *  data_size (size_t) [in]: OBSOLETE ignored field, csp packets have a fixed size now
- *
- * Returns:
- *  csp_packet_t *: Buffer (pointer to #csp_packet_t) or NULL if no buffers available
+ * @param[in] unused OBSOLETE ignored field, csp packets have a fixed size now
+ * @return Buffer pointer to #csp_packet_t or NULL if no buffers available
  */
 csp_packet_t * csp_buffer_get_isr(size_t unused);
 
 /**
  * Free buffer (from task context).
  *
- * Parameters:
- *  buffer (void *) [in]: buffer to free. NULL is handled gracefully.
+ * @param[in] buffer buffer to free. NULL is handled gracefully.
  */
 void csp_buffer_free(void *buffer);
 
 /**
  * Free buffer (from ISR context).
  *
- * Parameters:
- *  buffer (void *) [in]: buffer to free. NULL is handled gracefully.
+ * @oaram[in] buffer buffer to free. NULL is handled gracefully.
  */
 void csp_buffer_free_isr(void *buffer);
 
@@ -53,11 +45,8 @@ void csp_buffer_free_isr(void *buffer);
  * Clone an existing buffer.
  * The existing \a buffer content is copied to the new buffer.
  *
- * Parameters:
- *  buffer (void *) [in]: buffer to clone.
- *
- * Returns:
- *  void *: cloned buffer on success, or NULL on failure.
+ * @oaram[in] buffer buffer to clone.
+ * @return cloned buffer on success, or NULL on failure.
  */
 void * csp_buffer_clone(void *buffer);
 
@@ -65,8 +54,7 @@ void * csp_buffer_clone(void *buffer);
  * Return number of remaining/free buffers.
  * The number of buffers is set by csp_init().
  *
- * Returns:
- *  int: number of remaining/free buffers
+ * @return number of remaining/free buffers
  */
 int csp_buffer_remaining(void);
 
@@ -76,8 +64,7 @@ void csp_buffer_init(void);
  * Increase reference counter of buffer.
  * Use csp_buffer_free() to decrement
  *
- * Returns:
- *  size_t: CSP buffer data size.
+ * @return CSP buffer data size.
  */
 size_t csp_buffer_data_size(void);
 

include/csp/csp_cmp.h
@@ -153,16 +153,13 @@ struct csp_cmp_message {
 /**
  * Generic send management message request.
  *
- * Parameters:
- *  node (uint16_t) [in]: address of subsystem.
- *  timeout (uint32_t) [in]: timeout in mS to wait for reply..
- *  code (uint8_t) [in]: request code.
- *  msg_size (int) [in]: size of \a msg.
- *  msg (struct csp_cmp_message *) [in,out]: data.
- *
- * Returns:
- * int: #CSP_ERR_NONE on success, otherwise an error code.
-*/
+ *	@param[in] node address of subsystem.
+ *	@param[in] timeout timeout in mS to wait for reply..
+ *	@param[in] code request code.
+ *	@param[in] msg_size size of \a msg.
+ *	@param[in,out] msg data.
+ *	@return #CSP_ERR_NONE on success, otherwise an error code
+ */
 int csp_cmp(uint16_t node, uint32_t timeout, uint8_t code, int msg_size, struct csp_cmp_message *msg);
 
 /**
@@ -182,13 +179,10 @@ CMP_MESSAGE(CSP_CMP_ROUTE_SET_V2, route_set_v2)
 /**
  * Peek (read) memory on remote node.
  *
- * Parameters:
- *	node (uint16_t) [in]: address of subsystem.
- *	timeout (uint32_t) [in]: timeout in mS to wait for reply..
- *	msg (struct csp_cmp_message *) [in/out]: memory address and number of bytes to peek. (msg peeked/read memory)
- *
- * Returns:
- * 	int: #CSP_ERR_NONE on success, otherwise an error code.
+ *	@param[in] node address of subsystem.
+ *	@param[in] timeout timeout in mS to wait for reply..
+ *	@param[in/out] msg memory address and number of bytes to peek. (msg peeked/read memory)
+ *	@return #CSP_ERR_NONE on success, otherwise an error code.
  */
 static inline int csp_cmp_peek(uint16_t node, uint32_t timeout, struct csp_cmp_message *msg) {
 	return csp_cmp(node, timeout, CSP_CMP_PEEK, CMP_SIZE(peek) - sizeof(msg->peek.data) + msg->peek.len, msg);
@@ -197,13 +191,10 @@ static inline int csp_cmp_peek(uint16_t node, uint32_t timeout, struct csp_cmp_m
 /**
  * Poke (write) memory on remote node.
  *
- * Parameters:
- *	node (uint16_t) [in]: address of subsystem.
- *	timeout (uint32_t) [in]: timeout in mS to wait for reply..
- *	msg (struct csp_cmp_message *) [in]: memory address, number of bytes and the actual bytes to poke/write.
- *
- * Returns:
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ *	@param[in] node address of subsystem.
+ *	@param[in] timeout timeout in mS to wait for reply..
+ *	@param[in] msg memory address, number of bytes and the actual bytes to poke/write.
+ *	@return #CSP_ERR_NONE on success, otherwise an error code.
  */
 static inline int csp_cmp_poke(uint16_t node, uint32_t timeout, struct csp_cmp_message *msg) {
 	return csp_cmp(node, timeout, CSP_CMP_POKE, CMP_SIZE(poke) - sizeof(msg->poke.data) + msg->poke.len, msg);

include/csp/csp_iflist.h
@@ -14,20 +14,15 @@ extern "C" {
 /**
  * Add interface to the list.
  *
- * Parameters:
- *	face (csp_iface_t *) [in]: interface. The interface must remain valid as long
- *							   as the application is running.
- *
- * Returns
- *	int: #CSP_ERR_NONE on success, otherwise an error code.
+ * @param[in] iface The interface must remain valid as long as the application is running.
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_iflist_add(csp_iface_t * iface);
 
 /**
  * Remove interface from the list.
  *
- * Parameters:
- *	ifc (csp_iface_t *) [in]: Interface to remove. NULL will be gracefully handled.
+ * @param[in] ifc Interface to remove. NULL will be gracefully handled.
  */
 void csp_iflist_remove(csp_iface_t * ifc);
 
@@ -41,7 +36,7 @@ int csp_iflist_is_within_subnet(uint16_t addr, csp_iface_t * ifc);
 csp_iface_t * csp_iflist_get(void);
 
 /**
- *  Convert bytes to readable string
+ * Convert bytes to readable string
  */
 unsigned long csp_bytesize(unsigned long bytes, char *postfix);
 

commit 5ef9640bdd5393a5941709c0f2b5dc21aaf5624f

include: csp_crc32: Fix @return typo
@preturn typo was introduced by the commit 92f0e1008b0931057. Fix
them.

Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

include/csp/csp_crc32.h
@@ -23,7 +23,7 @@ typedef uint32_t csp_crc32_t;
  * Append CRC32 checksum to packet
  *
  * @param[in] packet CSP packet, must be valid.
- * @preturn #CSP_ERR_NONE on success, otherwise an error code.
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_crc32_append(csp_packet_t * packet);
 
@@ -31,7 +31,7 @@ int csp_crc32_append(csp_packet_t * packet);
  * Verify CRC32 checksum on packet.
  *
  * @param[in] packet CSP packet, must be valid.
- * @preturn #CSP_ERR_NONE on success, otherwise an error code.
+ * @return #CSP_ERR_NONE on success, otherwise an error code.
  */
 int csp_crc32_verify(csp_packet_t * packet);
 