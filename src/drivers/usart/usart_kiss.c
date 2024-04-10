4afa0b7c96100cf9dc1867187c7f423d3bd44e3d
KantaTamura <tkanta496@gmail.com>
2024-04-10 17:48:24 +0900

    Revert "drivers: usart: Remove csp_usart_fd_t typedef"
    This reverts commit 571b547511212e1a523eddabab5ade720d455719.

--- src/drivers/usart/usart_kiss.c
+++ src/drivers/usart/usart_kiss.c
@@ -12,7 +12,7 @@ typedef struct {
 	char name[CSP_IFLIST_NAME_MAX + 1];
 	csp_iface_t iface;
 	csp_kiss_interface_data_t ifdata;
-	int fd;
+	csp_usart_fd_t fd;
 } kiss_context_t;
 
 static int kiss_driver_tx(void * driver_data, const unsigned char * data, size_t data_length) {
--- src/drivers/usart/usart_linux.c
+++ src/drivers/usart/usart_linux.c
@@ -16,7 +16,7 @@
 typedef struct {
 	csp_usart_callback_t rx_callback;
 	void * user_data;
-	int fd;
+	csp_usart_fd_t fd;
 	pthread_t rx_thread;
 } usart_context_t;
 
@@ -53,7 +53,7 @@ static void * usart_rx_thread(void * arg) {
 	return NULL;
 }
 
-int csp_usart_write(int fd, const void * data, size_t data_length) {
+int csp_usart_write(csp_usart_fd_t fd, const void * data, size_t data_length) {
 
 	if (fd >= 0) {
 		int res = write(fd, data, data_length);
@@ -64,7 +64,7 @@ int csp_usart_write(int fd, const void * data, size_t data_length) {
 	return CSP_ERR_TX;  // best matching CSP error code.
 }
 
-int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callback, void * user_data, int * return_fd) {
+int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callback, void * user_data, csp_usart_fd_t * return_fd) {
 	if (rx_callback == NULL && return_fd == NULL) {
 		csp_print("%s: No rx_callback function pointer or return_fd pointer provided\n", __FUNCTION__);
 		return CSP_ERR_INVAL;
