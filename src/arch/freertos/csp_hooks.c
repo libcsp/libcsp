7e314418945350d5ae26ba7652039ef2033f98e1
dimitrovand <andrian.dimitrov94@gmail.com>
2024-12-16 05:56:32 +0200

    cmake: Add build support for FreeRTOS
    The CSP_SYSTEM_NAME variable is introduced to specify the kernel name
    since CMAKE_SYSTEM_NAME does not recognize FreeRTOS.
    This approach ensures flexibility for adding support for new kernels in
    the future.

---
+++ src/arch/freertos/csp_hooks.c
@@ -0,0 +1,17 @@
+#include <csp/csp_hooks.h>
+#include "csp_macro.h"
+
+__weak uint32_t csp_memfree_hook(void) {
+	return 0;
+}
+
+__weak unsigned int csp_ps_hook(csp_packet_t * packet) {
+	return 0;
+}
+
+__weak void csp_reboot_hook(void) {
+}
+
+__weak void csp_shutdown_hook(void) {
+}
+
