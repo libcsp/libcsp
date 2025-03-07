0b8c6ace295045c9893f3a6ff4ce1ba903611f85
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2025-03-07 19:46:46 +0900

    csp_route: Fix typo
    Fix "compatability" to "compatibility".
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- src/csp_route.c
+++ src/csp_route.c
@@ -61,7 +61,7 @@ static int csp_route_security_check(uint32_t security_opts, csp_iface_t * iface,
 
 	/* CRC32 verified packet */
 	if (packet->id.flags & CSP_FCRC32) {
-		/* Verify CRC32 (does not include header for backwards compatability with csp1.x) */
+		/* Verify CRC32 (does not include header for backwards compatibility with csp1.x) */
 		if (csp_crc32_verify(packet) != CSP_ERR_NONE) {
 			iface->rx_error++;
 			return CSP_ERR_CRC32;
@@ -74,7 +74,7 @@ static int csp_route_security_check(uint32_t security_opts, csp_iface_t * iface,
 #if (CSP_USE_HMAC)
 	/* HMAC authenticated packet */
 	if (packet->id.flags & CSP_FHMAC) {
-		/* Verify HMAC (does not include header for backwards compatability with csp1.x) */
+		/* Verify HMAC (does not include header for backwards compatibility with csp1.x) */
 		if (csp_hmac_verify(packet, false) != CSP_ERR_NONE) {
 			/* HMAC failed */
 			iface->autherr++;
