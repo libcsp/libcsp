c986444591f479c2c96fe09c42375fa38a07c634
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2025-04-21 23:56:40 +0900

    python: bindings: Add missing CSP_ERR_NOSYS constant
    The error constant CSP_ERR_NOSYS (-38) was defined in csp_error.h but was not
    exposed in the Python bindings.
    
    This commit adds the missing constant using PyModule_AddIntConstant, making it
    available to Python users of the module.
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- src/bindings/python/pycsp.c
+++ src/bindings/python/pycsp.c
@@ -1088,6 +1088,7 @@ PyMODINIT_FUNC PyInit_libcsp_py3(void) {
 	PyModule_AddIntConstant(m, "CSP_ERR_TX", CSP_ERR_TX);
 	PyModule_AddIntConstant(m, "CSP_ERR_DRIVER", CSP_ERR_DRIVER);
 	PyModule_AddIntConstant(m, "CSP_ERR_AGAIN", CSP_ERR_AGAIN);
+	PyModule_AddIntConstant(m, "CSP_ERR_NOSYS", CSP_ERR_NOSYS);
 	PyModule_AddIntConstant(m, "CSP_ERR_HMAC", CSP_ERR_HMAC);
 	PyModule_AddIntConstant(m, "CSP_ERR_CRC32", CSP_ERR_CRC32);
 	PyModule_AddIntConstant(m, "CSP_ERR_SFP", CSP_ERR_SFP);
