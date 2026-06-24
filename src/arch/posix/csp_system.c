7fa979f1d6b1c70f38c75fb4eba528b0be5e97e3
Alex Jackson <alex.jackson@warpware.co>
2026-06-24 16:03:06 -0600

    change function definition to weak

--- src/arch/posix/csp_system.c
+++ src/arch/posix/csp_system.c
@@ -1,4 +1,5 @@
 #include <csp/csp_hooks.h>
+#include "csp_macro.h"
 
 #include <unistd.h>
 #include <sys/sysinfo.h>
@@ -9,7 +10,7 @@
 #include <linux/reboot.h>
 #endif
 
-uint32_t csp_memfree_hook(void) {
+__weak uint32_t csp_memfree_hook(void) {
 	uint32_t total = 0;
 	struct sysinfo info;
 	sysinfo(&info);
@@ -17,12 +18,12 @@ uint32_t csp_memfree_hook(void) {
 	return total;
 }
 
-unsigned int csp_ps_hook(csp_packet_t * packet) {
+__weak unsigned int csp_ps_hook(csp_packet_t * packet) {
 	(void)packet; /* Avoid compiler warnings about unused parameter */
 	return 0;
 }
 
-void csp_reboot_hook(void) {
+__weak void csp_reboot_hook(void) {
 #ifdef __CYGWIN__
     csp_print("HALTED - Please reboot\n");
     while (true)
@@ -33,7 +34,7 @@ void csp_reboot_hook(void) {
 #endif
 }
 
-void csp_shutdown_hook(void) {
+__weak void csp_shutdown_hook(void) {
 #ifdef __CYGWIN__
     csp_print("HALTED - Please power off\n");
     while (true)
